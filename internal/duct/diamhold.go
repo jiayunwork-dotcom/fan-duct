a93d6d115ed2694a93ee1f5db6b54e739b493fab
“jiayun” <jiayun_work@163.com>
2026-08-26 15:51:44 +0800

    fix: init diamBinder map before recording validation errors

--- internal/duct/diamhold.go
+++ internal/duct/diamhold.go
@@ -4,7 +4,7 @@ type diamBinder struct {
 	byDiam map[float64]string
 }
 
-var liveDiam diamBinder
+var liveDiam = diamBinder{byDiam: make(map[float64]string)}
 
 func bindDiam(err error, diameter float64) error {
 	if err == nil {
